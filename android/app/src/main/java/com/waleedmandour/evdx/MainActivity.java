d65385adbdce4b1a8a79b6025960e2b51067e00c
Waleed Mandour <67415456+waleedmandour@users.noreply.github.com>
2026-06-22 10:48:37 +0400

    BYD Phase 2: Register BYDAutoPlugin in MainActivity

--- android/app/src/main/java/com/waleedmandour/evdx/MainActivity.java
+++ android/app/src/main/java/com/waleedmandour/evdx/MainActivity.java
@@ -2,10 +2,13 @@ package com.waleedmandour.evdx;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.waleedmandour.evdx.byd.BYDAutoPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Register BYD Auto plugin before super.onCreate
+        registerPlugin(BYDAutoPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
